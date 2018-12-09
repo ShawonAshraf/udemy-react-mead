a2be48e701fea2eb73720ee333fd33416c457d25
Shawon Ashraf <shawon13@live.com>
2018-12-10 02:32:05 +0600

    babel watch

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,9 +1,11 @@
 "use strict";
 
-console.log('App.js is running'); // JSX - JavaScript XML
+console.log('App.js is running');
+console.log('Live re-compile turned on!');
+console.log('Ayee it works!'); // JSX - JavaScript XML
 // switch default file handling in VSCode to JavaScript react for this to work
 // because - typescript linters
 
-var template = React.createElement("h1", null, "This is JSX from app.js!");
+var template = React.createElement("h1", null, "Indecison App");
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,11 +1,13 @@
 console.log('App.js is running');
+console.log('Live re-compile turned on!');
+console.log('Ayee it works!');
 
 
 // JSX - JavaScript XML
 // switch default file handling in VSCode to JavaScript react for this to work
 // because - typescript linters
 
-var template = <h1>This is JSX from app.js!</h1>;
+var template = <h1>Indecison App</h1>;
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
