10b8d53833e9095403a636eea4034bef70ac26d6
Shawon Ashraf <shawon13@live.com>
2018-12-10 03:54:46 +0600

    tag div fix

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -6,6 +6,6 @@ console.log('Ayee it works!'); // JSX - JavaScript XML
 // switch default file handling in VSCode to JavaScript react for this to work
 // because - typescript linters
 
-var template = React.createElement("h1", null, "Indecison App");
+var template = React.createElement("div", null, " ", React.createElement("h1", null, " Indecison App "), " ", React.createElement("p", null, "This is some paragraph text"), " ");
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.jsx
+++ indecision-app/src/app.jsx
@@ -7,7 +7,7 @@ console.log('Ayee it works!');
 // switch default file handling in VSCode to JavaScript react for this to work
 // because - typescript linters
 
-var template = <h1>Indecison App</h1>;
+var template = <div> <h1> Indecison App </h1> <p>This is some paragraph text</p> </div>;
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
