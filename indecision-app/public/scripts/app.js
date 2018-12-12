3b64af38916b448082f7a2ed9fe69dcb20177736
Shawon Ashraf <shawon13@live.com>
2018-12-13 02:58:24 +0600

    challenge 1 code

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -5,7 +5,21 @@ console.log('Live re-compile turned on!');
 console.log('Ayee it works!'); // JSX - JavaScript XML
 // switch default file handling in VSCode to JavaScript react for this to work
 // because - typescript linters
+// var template = (
+//   <div>
+//     <h1> Indecison App </h1>
+//     <p>This is some info</p>
+//   </div>
+// );
+// challenge code # 1
 
-var template = React.createElement("div", null, " ", React.createElement("h1", null, " Indecison App "), " ", React.createElement("p", null, "This is some paragraph text"), " ");
+var me = {
+  name: "Shawon",
+  city: "Dhaka",
+  dinner: "Kacchi"
+}; // use {} when rendering data from object
+// or json
+
+var template = React.createElement("div", null, React.createElement("h1", null, me.name), React.createElement("p", null, React.createElement("b", null, me.city)), React.createElement("p", null, "What did I have for dinner? ", React.createElement("b", null, me.dinner)));
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.jsx
+++ indecision-app/src/app.jsx
@@ -7,7 +7,33 @@ console.log('Ayee it works!');
 // switch default file handling in VSCode to JavaScript react for this to work
 // because - typescript linters
 
-var template = <div> <h1> Indecison App </h1> <p>This is some paragraph text</p> </div>;
-var appRoot = document.getElementById("app");
+// var template = (
+//   <div>
+//     <h1> Indecison App </h1>
+//     <p>This is some info</p>
+//   </div>
+// );
 
+// challenge code # 1
+const me = {
+  name: "Shawon",
+  city: "Dhaka",
+  dinner: "Kacchi"
+};
+
+// use {} when rendering data from object
+// or json
+var template = (
+  <div>
+    <h1>{me.name}</h1>
+    <p>
+      <b>{me.city}</b>
+    </p>
+    <p>
+      What did I have for dinner? <b>{me.dinner}</b>
+    </p>
+  </div>
+);
+
+var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
