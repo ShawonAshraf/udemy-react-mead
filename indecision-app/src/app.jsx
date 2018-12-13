9668103d8ab4ab324604249ac91cc1b8c80aa357
Shawon Ashraf <shawon13@live.com>
2018-12-13 21:08:30 +0600

    json parse error fix

--- indecision-app/src/app.jsx
+++ indecision-app/src/app.jsx
@@ -10,37 +10,37 @@ console.log('Ayee it works!');
 // Challenge code # 2
 const app = {
   title: 'Indecision App',
-  subtitle: 'This is some info'
+  subtitle: 'This is some info',
 };
 
-var template = (
-  <div>
-    <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
-  </div>
+const template = (
+    <div>
+      <h1>{app.title}</h1>
+      <p>{app.subtitle}</p>
+    </div>
 );
 
 // challenge code # 1
 const me = {
   name: "Shawon",
   city: "Dhaka",
-  dinner: "Kacchi"
+  dinner: "Kacchi",
 };
 
 // use {} when rendering data from object
 // or json, also known as JSX Expressions.
-var templateTwo = (
-  <div>
-    <h1>{me.name}</h1>
-    <p>
-      <b>{me.city}</b>
-    </p>
-    <p>
-      What did I have for dinner? <b>{me.dinner}</b>
-    </p>
-  </div>
+const templateTwo = (
+    <div>
+      <h1>{me.name}</h1>
+      <p>
+        <b>{me.city}</b>
+      </p>
+      <p>
+        What did I have for dinner? <b>{me.dinner}</b>
+      </p>
+    </div>
 );
 
-var appRoot = document.getElementById("app");
+const appRoot = document.getElementById("app");
 // ReactDOM.render(templateTwo, appRoot);
 ReactDOM.render(template, appRoot);
