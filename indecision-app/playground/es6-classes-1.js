f860de19f98824a1e266a0992b5971aae494db23
Shawon Ashraf <shawon13@live.com>
2018-12-15 02:17:07 +0600

    unead unkindled souls

--- indecision-app/playground/es6-classes-1.js
+++ indecision-app/playground/es6-classes-1.js
@@ -1,17 +1,45 @@
 class Person {
-  constructor(name = 'Anonymous') {
+  constructor(name = 'Anonymous', age = 0) {
     this.name = name;
+    this.age = age;
   }
 
   getGreeting() {
     return `Hi! ${this.name}`;
   }
+
+  getDescription() {
+    return `${this.name} is ${this.age} years old`;
+  }
 }
 
-const me = new Person('Shawon Ashraf');
-console.log(me);
-console.log(me.getGreeting());
+class Student extends Person {
+  constructor(name, age, major = 'Undecided') {
+    super(name, age);
+    this.major = major;
+  }
 
-const other = new Person();
-console.log(other);
-console.log(other.getGreeting());
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    return this.hasMajor() ? `${super.getDescription()} and majoring in ${this.major}` : super.getDescription();
+  }
+}
+
+class UndeadTraveller extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+    return `${greeting}, you've come far from ${this.homeLocation}. Do you need some souls?`;
+  }
+}
+
+const ashenOne = new UndeadTraveller('Ashen One', 9999, 'Lothric');
+console.log(ashenOne);
+console.log(ashenOne.getGreeting());
--- indecision-app/src/app.jsx
+++ indecision-app/src/app.jsx
@@ -90,4 +90,3 @@ const renderOptionsChange = () => {
 };
 // call
 renderOptionsChange();
-
