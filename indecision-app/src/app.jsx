d594dc5bf41dba3ba72453cbc06fdb196cbba7a2
Shawon Ashraf <shawon13@live.com>
2018-12-14 23:53:04 +0600

    disable button logic

--- indecision-app/src/app.jsx
+++ indecision-app/src/app.jsx
@@ -57,9 +57,6 @@ const onMakeDecision = () => {
   // console.log(randomNum);
 };
 
-// an array
-// const numbers = [55, 200, 156];
-
 // re render and update options array
 const renderOptionsChange = () => {
   const template = (
@@ -67,7 +64,6 @@ const renderOptionsChange = () => {
       <h1>{app.title}</h1>
       {app.subtitle ? <p>{app.subtitle}</p> : ''}
       <p>{app.options.length > 0 ? 'Here are your options' : 'You\'ve\ no options'}</p>
-      <p>{app.options.length}</p>
       {/* {
         numbers.map((num) => {
           return <p key={num}>Number : {num}</p>;
@@ -81,13 +77,13 @@ const renderOptionsChange = () => {
           )
         }
       </ol>
-      <button onClick={onMakeDecision}>What should I do?</button>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
       <button onClick={resetOptions}>Remove all</button>
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option"></input>
         <button>Add Option</button>
       </form>
-    </div>
+    </div >
   );
 
   ReactDOM.render(template, appRoot);
@@ -95,69 +91,3 @@ const renderOptionsChange = () => {
 // call
 renderOptionsChange();
 
-// template 2
-const me = {
-  name: "Shawon",
-  city: "Dhaka"
-};
-
-// get location via a function
-function getLocation(location) {
-  if (location) {
-    return (
-      <p>
-        Location: {location}
-      </p>
-    );
-  } else {
-    return 'Unknown';
-  }
-}
-
-// use {} when rendering data from object
-// or json, also known as JSX Expressions.
-const templateTwo = (
-  <div>
-    <h1>{me.name}</h1>
-    {getLocation(me.city)}
-  </div>
-);
-// ReactDOM.render(templateTwo, appRoot);
-
-
-// template 3 starts here
-// events and attrs
-// let count = 0;
-
-// // functions for manipulating counter
-// const addOne = () => {
-//   count += 1;
-//   renderCounter();
-// };
-
-// const minusOne = () => {
-//   count -= 1;
-//   renderCounter();
-// };
-
-// const reset = () => {
-//   count = 0;
-//   renderCounter();
-// };
-
-// ReactDOM.render(templateThree, appRoot);
-
-// const renderCounter = () => {
-//   const templateThree = (
-//     <div>
-//       <h1>Count : {count}</h1>
-//       <button onClick={addOne}>+1</button>
-//       <button onClick={minusOne}>-1</button>
-//       <button onClick={reset}>Reset</button>
-//     </div>
-//   );
-
-//   ReactDOM.render(templateThree, appRoot);
-// };
-
-// renderCounter();
