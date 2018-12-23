3e716706474b7667c8061e4038856a1279f89741
Shawon Ashraf <shawon13@live.com>
2018-12-24 04:37:13 +0600

    webpack loader config for jsx

---
+++ indecision-app/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader'
+    }],
+  },
+};
