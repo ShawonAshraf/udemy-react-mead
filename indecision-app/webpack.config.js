ff302e17740fbd939a4c981fd35a6fa49baf2b42
Shawon Ashraf <shawon13@live.com>
2018-12-24 22:29:41 +0600

    fixed webpack for jsx

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -2,15 +2,19 @@ const path = require('path');
 
 module.exports = {
   entry: './src/app.jsx',
+  mode: 'development',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
   },
   module: {
     rules: [{
-      test: /\.(jsx)$/,
-      exclude: /(node_modules)/,
-      loader: 'babel-loader'
-    }],
-  },
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      resolve: {
+        extensions: ['.jsx ']
+      }
+    }]
+  }
 };
