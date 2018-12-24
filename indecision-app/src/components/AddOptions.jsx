e7e91950d1bbd8c499c7b14936b22c3118712e2f
Shawon Ashraf <shawon13@live.com>
2018-12-24 22:33:33 +0600

    webpack checking done

--- indecision-app/src/components/AddOptions.jsx
+++ indecision-app/src/components/AddOptions.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default class AddOptions extends React.Component {
   // constructor
--- indecision-app/src/components/Option.jsx
+++ indecision-app/src/components/Option.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 // a single option
 // converted to functional components
 const Option = (props) => (
