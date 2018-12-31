597789f7abe1577ce99273e3ad84760c28cf0c2f
Shawon Ashraf <shawon13@live.com>
2018-12-31 22:59:09 +0600

    normalize

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// css reset using normalize
+import 'normalize.css/normalize.css';
 
 import IndecisionApp from "./components/IndecisionApp";
 import './styles/styles.scss';
