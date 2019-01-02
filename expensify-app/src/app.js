11b20a409149017d8b30852bfd2af0af7d275e7c
Shawon Ashraf <shawon13@live.com>
2019-01-03 00:40:38 +0600

    expensify

---
+++ expensify-app/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// css reset using normalize
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+ReactDOM.render(<p>This is boilerplate text</p>, document.getElementById("app"));
