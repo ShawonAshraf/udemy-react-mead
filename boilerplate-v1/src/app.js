60d6e66e90bfa66c2ca5059c6e492db1f8017619
Shawon Ashraf <shawon13@live.com>
2019-01-03 00:37:13 +0600

    boilerplate-1

--- boilerplate-v1/src/app.js
+++ boilerplate-v1/src/app.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // css reset using normalize
 import 'normalize.css/normalize.css';
-
-import IndecisionApp from "./components/IndecisionApp";
 import './styles/styles.scss';
 
 
-ReactDOM.render(<IndecisionApp></IndecisionApp>, document.getElementById("app"));
+ReactDOM.render(<p>This is boilerplate text</p>, document.getElementById("app"));
