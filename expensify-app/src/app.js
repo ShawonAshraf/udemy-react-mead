9e265e26d4c9b1de5b2db71aabc542da0da79965
Shawon Ashraf <shawon13@live.com>
2019-01-03 02:10:36 +0600

    s9 l78

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,8 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 // css reset using normalize
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashBoard = () => {
+  return (
+    <div>
+      This is my dashboard component
+    </div>
+  );
+};
 
-ReactDOM.render(<p>This is boilerplate text</p>, document.getElementById("app"));
+const AddExpensePage = () => {
+  return (
+    <div>
+      This is my add expense component
+    </div>
+  );
+};
+
+const EditExpensePage = () => {
+  return (
+    <div>
+      This is from edit expense page component
+    </div>
+  );
+};
+
+const HelpPage = () => {
+  return (
+    <div>
+      This is from help page
+    </div>
+  );
+};
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404!</h1>
+    </div>
+  );
+};
+
+// routes
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route exact={true} path="/" component={ExpenseDashBoard}/>
+        <Route exact={true} path="/create" component={AddExpensePage}/>
+        <Route exact={true} path="/edit" component={EditExpensePage}/>
+        <Route exact={true} path="/help" component={HelpPage}/>
+        <Route component={NotFoundPage}/>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
