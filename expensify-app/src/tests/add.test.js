b13f8ff4ee3878924c8f519e78c00b4ed9829315
Shawon Ashraf <shawon13@live.com>
2019-01-10 01:34:46 +0600

    jest fix for babel

--- expensify-app/src/tests/add.test.js
+++ expensify-app/src/tests/add.test.js
@@ -3,7 +3,9 @@ const add = (a, b) => a + b;
 test('should add two numbers', () => {
   const result = add(5, 6);
 
-  if (result !== 11) {
-    throw new Error(`Expected ${5 + 6} but got ${result}`);
-  }
+  // if (result !== 11) {
+  //   throw new Error(`Expected ${5 + 6} but got ${result}`);
+  // }
+
+  expect(result).toBe(11);
 });
