3cd0f45efee9cb79a8cd859f03d1b66b226d3925
Shawon Ashraf <shawon13@live.com>
2019-01-11 00:36:03 +0600

    empty expense list condition

--- expensify-app/src/components/ExpenseList.js
+++ expensify-app/src/components/ExpenseList.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import selectExpenses from '../selectors/expenses';
 import ExpenseListItem from './ExpenseListItem';
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
     {
-      props.expenses.map((expense) => {
-        return <ExpenseListItem {...expense} key={expense.id}/>;
-      })
+      props.expenses.length === 0 ? (
+        <p>No expenses</p>
+      ) : (
+          props.expenses.map((expense) => {
+            return <ExpenseListItem {...expense} key={expense.id} />;
+          })
+        )
     }
   </div>
 );
