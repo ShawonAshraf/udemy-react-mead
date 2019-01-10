df94867a74f5e3b7fa4f368f436b26ac616ce186
Shawon Ashraf <shawon13@live.com>
2019-01-11 02:35:35 +0600

    prod min js

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,29 +1,33 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  mode: 'development',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: path.join('scripts', 'bundle.js'),
-  },
-  module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+module.exports = (env) => {
+  // console.log('env', env);
+  const isProduction = env === 'production';
+  return {
+    entry: './src/app.js',
+    mode: 'development',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: path.join('scripts', 'bundle.js'),
+    },
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
