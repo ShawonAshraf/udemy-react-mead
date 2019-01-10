6caac301aa09ee5d805006eaa8ef8924b25da263
Shawon Ashraf <shawon13@live.com>
2019-01-11 03:01:28 +0600

    removed help page

--- expensify-app/src/routers/AppRouter.js
+++ expensify-app/src/routers/AppRouter.js
@@ -7,19 +7,17 @@ import Header from '../components/Header';
 import ExpenseDashBoard from '../components/ExpenseDashBoard';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
-      <Header/>
+      <Header />
       <Switch>
         <Route exact={true} path="/" component={ExpenseDashBoard} />
         <Route exact={true} path="/create" component={AddExpensePage} />
         <Route exact={true} path="/edit/:id" component={EditExpensePage} />
-        <Route exact={true} path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
