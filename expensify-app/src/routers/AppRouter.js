996981cac4fc57a441139076c00c9c20a52f384b
ShawonAshraf <shawon13@live.com>
2019-03-19 00:16:38 +0600

    166

--- expensify-app/src/routers/AppRouter.js
+++ expensify-app/src/routers/AppRouter.js
@@ -5,24 +5,25 @@ import createHistory from 'history/createBrowserHistory';
 
 
 // routes
-import Header from '../components/Header';
 import ExpenseDashBoard from '../components/ExpenseDashBoard';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 
+// private
+import PrivateRoute from './PrivateRoute';
+
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route exact={true} path="/" component={LoginPage} />
-        <Route exact={true} path="/dashboard" component={ExpenseDashBoard} />
-        <Route exact={true} path="/create" component={AddExpensePage} />
-        <Route exact={true} path="/edit/:id" component={EditExpensePage} />
+        <PrivateRoute exact={true} path="/dashboard" component={ExpenseDashBoard} />
+        <PrivateRoute exact={true} path="/create" component={AddExpensePage} />
+        <PrivateRoute exact={true} path="/edit/:id" component={EditExpensePage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
